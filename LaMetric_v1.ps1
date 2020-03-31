5fe81fdbe67f02237739e48cf2a5ee7ab6dfa0f5
Seidl Michael | base-IT <michael.seidl@baseit.at>
2020-03-31 07:17:09 +0200

    init

---
+++ LaMetric_v1.ps1
@@ -0,0 +1,67 @@
+﻿########################################################
+# Name: LaMetric.ps1                              
+# Creator: Michael Seidl aka Techguy                    
+# CreationDate: 28.12.2018
+# LastModified: 28.12.2018
+# Version: 1.0
+# Doc: http://www.techguy.at/tag/LaMetric/
+# PSVersion tested: 3 and 4
+#
+# Version 1.0 - RTM
+########################################################
+#
+# www.techguy.at                                        
+# www.facebook.com/TechguyAT                            
+# www.twitter.com/TechguyAT                             
+# michael@techguy.at 
+########################################################
+
+
+
+#Api Settings
+$LametricIP="192.168.1.199"
+$ApiKey="#####"
+
+#Alert settings
+$Priority="info" #"info, warning, critical
+$Icon="25675" #25674 = au2mator, 25675=Techguy
+$Text="www.techguy.at is my favorite Blog"
+ 
+$SoundCategory="notifications" #notifications, alarms
+$SoundId="notification4"
+$SoundRepeat="2"
+
+$DisplayCount=1
+
+
+#Code
+#Build JSon
+$json=@"
+{
+   "priority":"$Priority",
+   "model": {
+        "frames": [
+            {
+               "icon":$Icon,
+               "text":"$Text"
+    
+            }
+        ],
+        "sound": {
+            "category": "$SoundCategory",
+            "id": "$SoundId",
+            "repeat":$SoundRepeat
+        },
+        "cycles":$DisplayCount
+    }
+    
+}
+"@
+
+$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("dev:$ApiKey"))) 
+$header = @{ 
+    Authorization=("Basic {0}" -f $base64AuthInfo) 
+} 
+
+Invoke-RestMethod -Method POST -Uri ("http://"+$LametricIP+":8080/api/v2/device/notifications") -ContentType "application/json" -Headers $header -UseBasicParsing -Body $json
+
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# Lametric-Time-Send-Notification-with-PowerShell
+# Name: Lametric Time Notification
+    Creator: Michael Seidl aka Techguy
+    CreationDate: 23.03.2020
+    LastModified: 31.03.2020
+    Version: 1.0
+    Doc: http://www.techguy.at/how-to-alert-lametric-watch-with-powershell/
+    GitHub: https://github.com/Seidlm/Lametric-Time-Send-Notification-with-PowerShell
+
+
+# PowerShell Self Service Web Portal at www.au2mator.com/PowerShell
+
+
+# Description: 
+Send a Notification to your Lametric Time with Powershell
+See Blog Post for more details, on getting API key
+http://www.techguy.at/how-to-alert-lametric-watch-with-powershell/
+
+
+# Version 1.0 - RTM
+
+# Notes
+    www.techguy.at
+    www.facebook.com/TechguyAT
+    www.twitter.com/TechguyAT
+    michael@techguy.at
